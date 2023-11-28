20f3cf17290020cb4535cb6021ca8ac377299f4f
momiom <kazuki.k.bl8@gmail.com>
2023-11-29 01:12:30 +0900

    add prettier setting

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
 };
 
 export default config;
